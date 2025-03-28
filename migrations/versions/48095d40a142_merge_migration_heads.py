0627d7eb530a0092a59f04a663822bf1ac8f6b4a
William Swain <w.swain@ucl.ac.uk>
2025-03-28 10:34:17 +0000

    Merge Alembic migration heads to resolve branching history

---
+++ migrations/versions/48095d40a142_merge_migration_heads.py
@@ -0,0 +1,24 @@
+"""Merge migration heads
+
+Revision ID: 48095d40a142
+Revises: 
+Create Date: 2025-03-28 10:33:17.413792
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '48095d40a142'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
